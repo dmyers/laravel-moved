f4f4ceb2491d43095ab654ef41e6b6a01ead4a07
Derek Myers <arcticpro@gmail.com>
2019-12-26 20:38:10 -0600

    Fixed firing event to use dispatch method for redirects

--- src/Dmyers/Moved/MovedServiceProvider.php
+++ src/Dmyers/Moved/MovedServiceProvider.php
@@ -2,39 +2,38 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class MovedServiceProvider extends ServiceProvider {
+class MovedServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->router->group(['namespace' => 'Dmyers\Moved'], function($router) {
+            include __DIR__.'/../../routes.php';
+        });
+        
+        $this->publishes([
+            __DIR__.'/../../config/moved.php' => config_path('moved.php'),
+        ]);
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->app->router->group(['namespace' => 'Dmyers\Moved'], function($router) {
-			include __DIR__.'/../../routes.php';
-		});
-		
-		$this->publishes([
-			__DIR__.'/../../config/moved.php' => config_path('moved.php'),
-		]);
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
-
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
